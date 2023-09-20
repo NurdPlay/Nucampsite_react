d3d3e5b867debb41c6f3ae467fcd3a22dd7446a5
John Elliott <j.elliott1700@gmail.com>
2023-09-20 11:21:22 -0700

    commiting everything

---
+++ src/features/counter/campsites/CampsiteCard.js
@@ -0,0 +1,22 @@
+import { Card } from "reactstrap";
+import CardImg from "reactstrap";
+import CardImgOverlay from "reactstrap";
+import CardTitle from "reactstrap";
+
+const CampsiteCard = (props) => {
+    return (
+        <Card>
+            <CardImg 
+                width='100%'
+                src={props.campsite.image}
+                alt={props.campsite.name}
+            />
+
+            <CardImgOverlay>
+                <CardTitle>
+                    {props.campsite.name}
+                </CardTitle>
+            </CardImgOverlay>
+        </Card>
+    )
+}
